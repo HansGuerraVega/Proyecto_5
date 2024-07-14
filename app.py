56ae07697bee62ed235686f76757d73a42d7a911
HansGuerraVega <hans.guerra.vega@gmail.com>
2024-07-13 23:45:00 -0600

    main

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-print('En esta pagina encontrarás dos botones que muestran un histograma/n')
-print('y una gráfica de dispersión que muestran a su manera como cambia el') 
-print('precio dependiendo del kilometraje')
+print('En esta pagina encontrarás dos botones, uno grafica un histograma de') 
+print('la cantidad de coches con ciertos kilómetris y el otro una gráfica de') 
+print('dispersión de cómo cambia el precio dependiendo del kilometraje')
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
@@ -22,7 +22,7 @@ if hist_button: # al hacer clic en el botón
 
 if dispersion_button: # al hacer clic en el botón
     # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    st.write('Creación de un una gráfica de dipersión para el conjunto de datos de anuncios de venta de coches')
     
     # crear un gráfico de dispersión
     fig = px.scatter(car_data, x="odometer", y="price") 
